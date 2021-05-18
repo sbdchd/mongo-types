8fed175ce6ea525d2eb5af556a89d96f16663b06
Glenn Olsson <glennholsson@gmail.com>
2021-05-18 17:22:18 +0200

    Add remaining parameters to register_connection

--- typings/mongoengine/__init__.pyi
+++ typings/mongoengine/__init__.pyi
@@ -6,10 +6,10 @@ from mongoengine.document import Document, DynamicDocument, EmbeddedDocument
 from mongoengine.errors import DoesNotExist, NotUniqueError, ValidationError
 from mongoengine.queryset.queryset import QuerySet
 from mongoengine.queryset.visitor import Q
-from pymongo import MongoClient
+from pymongo import MongoClient, ReadPreference
 
 def connect(name: str, alias: str = ..., host: Optional[str] = ...) -> MongoClient: ...
-def register_connection(host: str, alias: str) -> None: ...
+def register_connection(alias: str, db: str = None, name: str = None, host: str = None, port: int = None, read_preference: ReadPreference = ReadPreference.Primary, username: str = None, password: str = None, authentication_source: str = None, authentication_mechanism: str = None, **kwargs) -> None: ...
 
 __all__ = [
     "Q",
