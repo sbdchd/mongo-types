767414660ee80895a5de0283a26108072c4a660f
phil-airship <90795969+phil-airship@users.noreply.github.com>
2021-11-18 19:16:42 -0600

    Build Out Connection Interface (#61)
    Used mypy stubgen, and then manually modified to align with proper types and repo formatting.
    Previously lacked the public connection interface

--- mongoengine-stubs/connection.pyi
+++ mongoengine-stubs/connection.pyi
@@ -1,7 +1,47 @@
-from typing import Dict, Union
+from typing import Any
 
-from pymongo import MongoClient
+from pymongo import MongoClient, ReadPreference
+from pymongo.database import Database
 
-_connections: Dict[str, Union[MongoClient, object]]
+DEFAULT_CONNECTION_NAME: str
+DEFAULT_DATABASE_NAME: str
 
-__all__ = ["_connections"]
+class ConnectionFailure(Exception): ...
+
+def register_connection(
+    alias: str,
+    db: str | None = ...,
+    name: str | None = ...,
+    host: str | None = ...,
+    port: str | None = ...,
+    read_preference: ReadPreference = ...,
+    username: str | None = ...,
+    password: str | None = ...,
+    authentication_source: str | None = ...,
+    authentication_mechanism: str | None = ...,
+    **kwargs: Any,
+) -> None: ...
+def disconnect(alias: str | None = ...) -> None: ...
+def disconnect_all() -> None: ...
+def get_connection(
+    alias: str | None = ...,
+    reconnect: bool = ...,
+) -> MongoClient: ...
+def get_db(alias: str | None = ..., reconnect: bool = ...) -> Database: ...
+def connect(
+    db: str | None = ...,
+    alias: str | None = ...,
+    **kwargs: Any,
+) -> MongoClient: ...
+
+__all__ = [
+    "DEFAULT_CONNECTION_NAME",
+    "DEFAULT_DATABASE_NAME",
+    "ConnectionFailure",
+    "connect",
+    "disconnect",
+    "disconnect_all",
+    "get_connection",
+    "get_db",
+    "register_connection",
+]
